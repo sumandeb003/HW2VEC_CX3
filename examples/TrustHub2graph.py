19aa4b2e4b89aadb72a8e8d3eeee93264ea69063
Deb <sdeb2@ic.ac.uk>
2024-09-04 22:00:05 +0800

    edited TrustHub2graph.py

--- examples/TrustHub2graph.py
+++ examples/TrustHub2graph.py
@@ -3,6 +3,7 @@ sys.path.append(os.path.dirname(sys.path[0]))
 from hw2vec.hw2graph import *
 from hw2vec.config import Config
 from pathlib import Path
+import glob, shutil
 
 def delete_all_dotptfiles_in_all_subdirectories(directory_path):
 	for subdir in os.listdir(directory_path):
@@ -13,10 +14,17 @@ def delete_all_dotptfiles_in_all_subdirectories(directory_path):
 				print('Deleting: ', circuit_path)
 				os.remove(circuit_path)
 
+def copy_all_dotptfiles(src_folder, dst_folder):
+    if not os.path.exists(dst_folder):
+        os.makedirs(dst_folder)
+    files = glob.iglob(os.path.join(circuit_path, '*.pt'))
+    for file in files:
+        shutil.copy2(file, dst_folder)
 
-def TrustHub_to_graph(cfg, hw_design_dir_path):
+
+def TrustHub_to_graph(cfg, circuit_path):
     hw2graph = HW2GRAPH(cfg)
-    hw_design_path = hw2graph.preprocess(hw_design_dir_path) #flatten all .v files to one .v file, remove comments, remove underscores, rename as topModule.v
+    hw_design_path = hw2graph.preprocess(circuit_path) #flatten all .v files to one .v file, remove comments, remove underscores, rename as topModule.v
     hardware_nxgraph = hw2graph.process(hw_design_path) #generate AST/DFG (JSON format) of the topModule.v
     data_proc = DataProcessor(cfg)
     data_proc.process(hardware_nxgraph)#normalize the graph and create node-feature vectors X and adjacency matrix A
@@ -26,10 +34,10 @@ def TrustHub_to_graph(cfg, hw_design_dir_path):
     print (f'TrustHub_to_graph: data - {data}')
     hw_design = str(hw_design_path).split("/")[-2].replace('_','-')
     if "TjFree" == str(hw_design_path).split("/")[-3]:
-    	data[0].label = 'TJFREE'
+    	data[0].label = 'TjFree'
     else:
-    	data[0].label = 'TJIN'
-    save_path = os.path.join(hw_design_dir_path, f'{hw_design}_topModule_{data[0].label}_{cfg.graph_type}.pt')
+    	data[0].label = 'TjIn'
+    save_path = os.path.join(circuit_path, f'{hw_design}_{data[0].label}_topModule_{cfg.graph_type}.pt')
     torch.save(data[0], save_path)
 
 if __name__ == '__main__':
@@ -42,25 +50,36 @@ if __name__ == '__main__':
     #log = open('TrustHub2graph.log', 'a')
     #sys.stdout = log
     #delete all previous .pt files in all sub-directories of directory_path
-    directory_path = '../assets/datasets/MyTrustHub4GraphGPS/TjFree' #like, path to TjFree or TjIn
+    directory_path = '../assets/datasets/MyTrustHub4GraphGPS' #like, path to TjFree or TjIn
     print('starting: delete_all_dotptfiles_in_all_subdirectories(directory_path)')
+    dst_folder = os.path.join(directory_path,'TrustHubGraphDataset')
     delete_all_dotptfiles_in_all_subdirectories(directory_path)
     print ('done: delete_all_dotptfiles_in_all_subdirectories(directory_path)')
     #create new .pt files
     cfg = Config(sys.argv[1:]) #because of the following 4 lines, the statement 'cfg = Config(sys.argv[1:]) has no use
     #iterate through all folders in TjFree
-    for subdir in os.listdir(directory_path):
-    	subdir_path = Path(os.path.join(directory_path,subdir))
-    	print('===================================================================')
-    	print (f'TrustHub_to_graph: circuit - {subdir_path}')
-    	#do both AST and DFG
-    	for graphtype in ['AST', 'DFG']:
-    		cfg.graph_type = graphtype
-    		print (f'TrustHub_to_graph: graphtype - {cfg.graph_type}')
-    		try:
-    			TrustHub_to_graph(cfg, subdir_path)
-    		except Exception as error:
-    			print("ERROR:	", type(error).__name__, "–", error)
-    	print('===================================================================')
+    for type in ['TjFree', 'TjIn']:
+        type_path = os.join(directory_path, type)
+        for circuit in os.listdir(type_path):
+            circuit_path = Path(os.path.join(type_path,circuit))
+            print('===================================================================')
+            print (f'TrustHub_to_graph: circuit - {circuit_path}')
+            #do both AST and DFG
+            for graphtype in ['AST', 'DFG']:
+                cfg.graph_type = graphtype
+                print (f'TrustHub_to_graph: graphtype - {cfg.graph_type}')
+                try:
+                    TrustHub_to_graph(cfg, circuit_path)
+                except Exception as error:
+                    print("ERROR:	", type(error).__name__, "–", error)
+            
+            copy_all_dotptfiles(circuit_path, dst_folder)
+            print('===================================================================')
+    
+    #zip all the graphs
+    zippedfile = os.path.join(directory_path, 'TrustHubGraphDataset.zip')
+    cmd = 'zip ' + zippedfile + ' -r ' + dst_folder
+    os.system(cmd)
+    
 
     #log.close()
