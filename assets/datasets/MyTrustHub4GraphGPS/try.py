fd64efbbf19a392095e25d0163e6d4cb87f139f6
Deb <sdeb2@ic.ac.uk>
2024-09-07 22:58:02 +0800

    edited TrustHub2graph.py

--- assets/datasets/MyTrustHub4GraphGPS/try.py
+++ assets/datasets/MyTrustHub4GraphGPS/try.py
@@ -1,2 +1,9 @@
+import os
+import torch
+from torch_geometric.data import download_url, extract_zip
 
+id = '1mgBILYWXRyY9jAXeslmpKgwilP1etvSs'
+url = f'https://drive.usercontent.google.com/download?id={id}&confirm=t'
 
+path = download_url(url, os.path.join(os.getcwd(),'raw'))
+extract_zip('delete.zip',os.path.join(os.getcwd(),'raw')
